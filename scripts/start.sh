ca09b663c0b11f281a09cec462a203b97206051c
nolan <nolan.martin1@hotmail.com>
2025-04-28 15:06:31 +0200

    GeoServer + Geonature

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,8 @@ sudo apt update && sudo apt install -y \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release
+    lsb-release \
+    postgresql-client
 
 # DOCKER
 sudo mkdir -p /etc/apt/keyrings
@@ -14,21 +15,37 @@ echo \
   | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
 
-# POSTGIS
-sudo docker pull postgis/postgis:17-3.5
-sudo docker run --name postgis -e POSTGRES_USER=geonature -e POSTGRES_PASSWORD=${password_postgres} -e POSTGRES_DB=geonature_db -d postgres
+# GEOSERVER ?
+sudo mkdir -p /opt/geoserver_data
+sudo mkdir -p /opt/postgis_data
+sudo chmod -R 777 /opt/geoserver_data /opt/postgis_data
 
-# SETUP GEONATURE
-sudo echo "LANG=fr_FR.UTF-8" | sudo tee -a /etc/default/locale > /dev/null
-sudo source /etc/default/locale
-sudo update-locale LANG=fr_FR.UTF-8
+sudo git clone https://github.com/geoserver/docker.git /opt/geoserver-docker
 
-sudo docker pull docker.osgeo.org/geoserver:2.27.0
-sudo docker run -d -p 80:8080 -e GEOSERVER_ADMIN_PASSWORD=${password_geoserver} -e GEOSERVER_ADMIN_USER=admin docker.osgeo.org/geoserver:2.27.0 --name geoserver 
+sudo sed -i "s/^\(\s*POSTGRES_PASSWORD:\s*\)geoserver/\1${password_postgres}/" /opt/geoserver-docker/docker-compose-demo.yml
+sudo sed -i "s/^\(\s*-\s*POSTGRES_PASSWORD=\)geoserver/\1${password_postgres}/" /opt/geoserver-docker/docker-compose-demo.yml
+sudo sed -i "s|\(^\s*-\s*\)\./postgis/postgresql_data|\1/opt/postgis_data|g" /opt/geoserver-docker/docker-compose-demo.yml
+sudo sed -i "s|\(^\s*-\s*\)\./geoserver_data\(:/opt/geoserver_data/:Z\)|\1/opt/geoserver_data\2|" /opt/geoserver-docker/docker-compose-demo.yml
+sudo sed -i "s/^\(\s*POSTGRES_PASSWORD:\s*\)geoserver/\1${password_postgres}/" /opt/geoserver-docker/docker-compose-demo.yml
 
-#Create a test file to test the succes of the script
-touch /tmp/myfile
-echo ${password_postgres} > /tmp/myfile
+# GEONATURE
+sudo git clone https://github.com/PnX-SI/GeoNature-Docker-services.git /opt/geonature-docker
+
+sudo chmod -R 777 /opt/geonature-docker
+sudo cp /opt/geonature-docker/.env.sample /opt/geonature-docker/.env
+
+sudo sed -i 's/^ACME_EMAIL=".*"/ACME_EMAIL="nolan.martin@1004.tech"/' /opt/geonature-docker/.env
+sudo sed -i 's/^ACME_EMAIL=".*"/ACME_EMAIL="nolan.martin@1004.tech"/' /opt/geonature-docker/.env
+sudo sed -i "s/^POSTGRES_PASSWORD=".*"/POSTGRES_PASSWORD=\"${password_postgres}\"/" /opt/geonature-docker/.env
+sudo sed -i 's/^BASE_PROTOCOL=".*"/BASE_PROTOCOL="http"/' /opt/geonature-docker/.env
+
+sudo /opt/geonature-docker/init-config.sh
+
+
+# START DOCKER
+sudo docker compose -f /opt/geonature-docker/docker-compose.yml up -d
+sudo docker-compose -f /opt/geoserver-docker/docker-compose-demo.yml up --build
+
 
--- vm.tf
+++ vm.tf
@@ -99,6 +99,28 @@ resource "azurerm_network_security_group" "cenTest" {
     access                     = "Allow"
     protocol                  = "Tcp"
     source_port_range         = "*"
+    destination_port_range    = "80"
+    source_address_prefix     = "*"
+    destination_address_prefix = "*"
+  }
+   security_rule {
+    name                       = "AllowAnyHTTPSInbound"
+    priority                   = 102
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                  = "Tcp"
+    source_port_range         = "*"
+    destination_port_range    = "443"
+    source_address_prefix     = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "AllowAnyHTTP8080Inbound"
+    priority                   = 103
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                  = "Tcp"
+    source_port_range         = "*"
     destination_port_range    = "8080"
     source_address_prefix     = "*"
     destination_address_prefix = "*"
